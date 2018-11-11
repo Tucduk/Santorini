630994516f670e6b641a858eaa9c34a04a7b0548
Unknown <thomasbfjfn@gmail.com>
2018-11-11 20:13:41 +0100

    Update

--- src/sample/Plateau.java
+++ src/sample/Plateau.java
@@ -6,32 +6,41 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-
 import javafx.stage.Stage;
-
 import java.util.Random;
-
 
 public class Plateau extends Application {
 
     @Override
     public void start(Stage primaryStage){
-        int i = 5;
+        int I = 5;
+
         GridPane grid = new GridPane();
+        Controller controller;
+        Button button;
 
-        Image sol = new Image ("/imageSantorini/Sol.png",200,200,true,true);
-        Image sol2 = new Image ("/imageSantorini/Sol2.png",200,200,true,true);
-        Image sol3 = new Image ("/imageSantorini/Sol3.png",200,200,true,true);
-        Image sol4 = new Image ("/imageSantorini/Sol4.png",200,200,true,true);
+        Image sol = new Image ("/imageSantorini/Sol.png",150,150,true,true);
+        Image sol2 = new Image ("/imageSantorini/Sol2.png",150,150,true,true);
+        Image sol3 = new Image ("/imageSantorini/Sol3.png",150,150,true,true);
+        Image sol4 = new Image ("/imageSantorini/Sol4.png",150,150,true,true);
 
-        Image[] image = {sol, sol2, sol3, sol4};
+        Image base = new Image ("/imageSantorini/Maison.png",150,150,true,true);
+        Image base2 = new Image ("/imageSantorini/Maison2.png",150,150,true,true);
+        Image base3 = new Image ("/imageSantorini/Maison3.png",150,150,true,true);
+        Image base4 = new Image ("/imageSantorini/Maison4.png",150,150,true,true);
+
+        Image[] imageBase = new Image[]{base, base2, base3, base4};
+
+        Image[] image = new Image[]{sol, sol2, sol3, sol4};
 
         //Creation de la grille
-        for (int row = 0; row < i ; row++) {
-            for (int col = 0; col < i; col++) {
-                Button button = new Button();
-                button.setMaxSize(200,200);
-                button.setMinSize(200,200);
+        for (int row = 0; row < I ; row++) {
+            for (int col = 0; col < I; col++) {
+                button = new Button();
+                controller = new Controller(button);
+                button.setMaxSize(150,150);
+                button.setMinSize(150,150);
+                button.setId("button" + String.valueOf(row) + "-" + String.valueOf(col));
 
                 //image pour les bords
                 if (row == 4){
@@ -55,7 +64,7 @@ public class Plateau extends Application {
             }
         }
 
-        Scene scene = new Scene(grid, i*200, i*200);
+        Scene scene = new Scene(grid, I*150, I*150);
         primaryStage.setTitle("Santorini");
         primaryStage.setScene(scene);
         primaryStage.show();
